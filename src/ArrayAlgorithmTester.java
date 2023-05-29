d895b6d8fee478ae8ba6a2152dd30d43b8136adc
pleiotropy <angela.m.joseph+github@gmail.com>
2023-05-28 22:48:45 -0400

    U6L6 Array Algorithms

--- src/ArrayAlgorithmTester.java
+++ src/ArrayAlgorithmTester.java
@@ -4,34 +4,120 @@ public class ArrayAlgorithmTester
 {
     public static void main(String[] args)
     {
-        // TEST longestString
-        String[] strings = {"hello", "hola", "hi", "apple", "why?"};
-        System.out.println(ArrayAlgorithms.longestString(strings));
+//        // TEST longestString
+//        String[] strings = {"hello", "hola", "hi", "apple", "why?"};
+//        System.out.println(ArrayAlgorithms.longestString(strings));
+//
+//        // original array NOT modified
+//        System.out.println(Arrays.toString(strings));
+//
+//        System.out.println("----");
+//
+//        String[] strings2 = {"cut", "boy", "mom", "she"};
+//        System.out.println(ArrayAlgorithms.longestString(strings2));
+//
+//        // original array NOT modified
+//        System.out.println(Arrays.toString(strings2));
+//
+//        System.out.println("----");
+//
+//        String[] strings3 = {"M", "DA", "B", "FAD"};
+//        System.out.println(ArrayAlgorithms.longestString(strings3));
+//
+//        // original array NOT modified
+//        System.out.println(Arrays.toString(strings3));
+//
+//        // TEST addExclamation
+//        String[] words = {"hello", "hola", "hi", "what's up?"};
+//        ArrayAlgorithms.makeUppercase(words);
+//
+//        // original word list IS modified
+//        System.out.println(Arrays.toString(words));
+//
+//        // TEST containsPositive
+//        int[] nums = {-6, -4, 0, -3, 1, -2, 5};
+//        boolean positive = ArrayAlgorithms.containsPositive(nums);
+//        System.out.println(positive);
+//
+//        // original numbers list NOT modified
+//        System.out.println(Arrays.toString(nums));
+//
+//        System.out.println("----");
+//
+//        int[] nums2 = {-6, -4, 0, -3, -1, -2, -5};
+//        boolean positive2 = ArrayAlgorithms.containsPositive(nums2);
+//        System.out.println(positive2);
+//
+//        // original numbers list NOT modified
+//        System.out.println(Arrays.toString(nums2));
+//
+//        // TEST containsString
+//        String[] strings = {"hello", "hole", "Adam", "Apple", "why?"};
+//        boolean hasStr = ArrayAlgorithms.containsString(strings, "y");
+//        System.out.println(hasStr);
+//
+//        // original array NOT modified
+//        System.out.println(Arrays.toString(strings));
+//
+//        System.out.println("----");
+//
+//        boolean hasStr2 = ArrayAlgorithms.containsString(strings, "LE");
+//        System.out.println(hasStr2);
+//
+//        boolean hasStr3 = ArrayAlgorithms.containsString(strings, "ap");
+//        System.out.println(hasStr3);
+//
+//        boolean hasStr4 = ArrayAlgorithms.containsString(strings, "hola");
+//        System.out.println(hasStr4);
+//
+//        // TEST makeLowercase
+//        String[] strings = {"HELLO", "Hole", "Adam", "booyAH", "WHY?"};
+//        String[] lower = ArrayAlgorithms.makeLowercase(strings);
+//        System.out.println(Arrays.toString(lower));
+//
+//        // original array NOT modified
+//        System.out.println(Arrays.toString(strings));
+//
+//        int[] arr1= {2, 5, 6, 1, 0};
+//        int[] arr2 = {1, 7, 6, -2, 8};
+//        int[] maximums = ArrayAlgorithms.arrayMaximums(arr1, arr2);
+//        System.out.println(Arrays.toString(maximums));
+//
+//        // original arrays NOT modified
+//        System.out.println(Arrays.toString(arr1));
+//        System.out.println(Arrays.toString(arr2));
+//
+//        // TEST endsInExclamations
+//        String[] strings = {"hello!", "!!!whoa", "sad", "Adam!", "What! No way", "Why?!"};
+//        int numExclamations = ArrayAlgorithms.endsInExclamations(strings);
+//        System.out.println(numExclamations);
+//
+//        // original array NOT modified
+//        System.out.println(Arrays.toString(strings));
+//
+//        // TEST countConsecutiveDoubles
+//        int[] nums = {4, 7, 7, 3, 2, 2, 4, 4, 4, 6, 4, 6, 6};
+//        int numDoubles = ArrayAlgorithms.countConsecutiveDoubles(nums);
+//        System.out.println(numDoubles);
+//
+//        // original array NOT modified
+//        System.out.println(Arrays.toString(nums));
+//
+//        // TEST shiftLeft
+//        int[] nums = {5, 1, 3, 4, 7};
+//        int[] shiftedLeftArr = ArrayAlgorithms.shiftLeft(nums);
+//        System.out.println(Arrays.toString(shiftedLeftArr));
+//
+//        // original array NOT modified
+//        System.out.println(Arrays.toString(nums));
 
-        // original array NOT modified
-        System.out.println(Arrays.toString(strings));
+        // TEST shiftLeftModify
+        int[] nums = {5, 1, 3, 4, 7};
+        ArrayAlgorithms.shiftLeftModify(nums);
 
-        System.out.println("----");
+        // original array IS modified
+        System.out.println(Arrays.toString(nums));
 
-        String[] strings2 = {"cut", "boy", "mom", "she"};
-        System.out.println(ArrayAlgorithms.longestString(strings2));
 
-        // original array NOT modified
-        System.out.println(Arrays.toString(strings2));
-
-        System.out.println("----");
-
-        String[] strings3 = {"M", "DA", "B", "FAD"};
-        System.out.println(ArrayAlgorithms.longestString(strings3));
-
-        // original array NOT modified
-        System.out.println(Arrays.toString(strings3));
-
-        // TEST addExclamation
-        String[] words = {"hello", "hola", "hi", "what's up?"};
-        ArrayAlgorithms.makeUppercase(words);
-
-        // original word list IS modified
-        System.out.println(Arrays.toString(words));
     }
 }
--- src/ArrayAlgorithms.java
+++ src/ArrayAlgorithms.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class ArrayAlgorithms {
     /** Prints each String in wordList, on its own line, followed by “!”.
      *  Does NOT mutate (modify) wordList.
@@ -208,4 +210,193 @@ public class ArrayAlgorithms {
         }
     }
 
+    /** Returns true if ANY of the ints in numList are positive, otherwise,
+     *  returns false.
+     *
+     *  Does NOT mutate (modify) numList.
+     *  PRECONDITION: numList.length > 0
+     *
+     *  @param numList  original array of ints; does not get modified
+     *  @return  true if any element of numList is positive, false otherwise
+     */
+    public static boolean containsPositive(int[] numList)
+    {
+        for (int num : numList)
+        {
+            if (num > 0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /** Returns true if ANY of the Strings in stringList contain searchStr,
+     *  otherwise, returns false. Your method should NOT be case sensitive, e.g.
+     *  searching for "ma" should be found in the String "Maddox" and searching
+     *  for "MA" should be found in String "mad"
+     *
+     *  Does NOT mutate (modify) stringList.
+     *  PRECONDITION: stringList.length > 0
+     *
+     *  @param stringList  original array of Strings; does not get modified
+     *  @param searchStr  the String to search each element of stringList
+     *  @return  true if any element of stringList contains searchStr, otherwise
+     *           returns false
+     */
+    public static boolean containsString(String[] stringList, String searchStr)
+    {
+        for (String str : stringList)
+        {
+            if (str.toLowerCase().indexOf(searchStr.toLowerCase()) != -1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /** Returns a NEW array of Strings that contains all words from the original
+     *  array of strings in lowercase.
+     *
+     *  Does NOT mutate (modify) original wordList
+     *  PRECONDITION: wordList.length > 0
+     *
+     *  @param wordList  original array of Strings, not modified by method
+     *  @return  new array of Strings in all lowercase
+     */
+
+    public static String[] makeLowercase(String[] wordList)
+    {
+        String[] lowercaseWordList = new String[wordList.length];
+        int i = 0;
+        for (String originalWord : wordList)
+        {
+            lowercaseWordList[i] = originalWord.toLowerCase();
+            i++;
+        }
+        return lowercaseWordList;
+    }
+
+    /** Returns a NEW array of ints that contains the higher value of each pair
+     *  of elements at the same index in two other equally-sized arrays of ints
+     *  For example, if intArr1 = {2, 5, 6, 1, 0} and intArr2 = {1, 7, 6, -2, 8},
+     *  this method returns the array {2, 7, 6, 1, 8}, where each value is the
+     *  higher of the two at that respective index in the two original arrays
+     *
+     *  Does NOT mutate (modify) EITHER array
+     *  PRECONDITION: intArr1.length == intArr2.length
+     *
+     *  @param intArr1  first array of ints
+     *  @param intArr2  second array of ints, having the same length as intArr1
+     *  @return  new array of ints representing the higher values
+     */
+
+    public static int[] arrayMaximums(int[] intArr1, int[] intArr2)
+    {
+        int[] maxValues = new int[intArr1.length];
+        int i = 0;
+        for (int int1 : intArr1)
+        {
+            if (int1 >= intArr2[i])
+            {
+                maxValues[i] = int1;
+            }
+            else
+            {
+                maxValues[i] = intArr2[i];
+            }
+            i++;
+        }
+        return maxValues;
+    }
+
+    /** Returns the number of Strings in stringList that have an exclamation point
+     *  ("!") at the end.
+     *
+     *  Does NOT mutate (modify) stringList.
+     *  PRECONDITION: stringList.length > 0
+     *
+     *  @param stringList  original array of Strings; does not get modified
+     *  @return  number of Strings in stringList that end in "!"
+     */
+    public static int endsInExclamations(String[] stringList)
+    {
+        int exclamationCount = 0;
+        for (String str : stringList)
+        {
+            if (str.indexOf("!") == str.length()-1)
+            {
+                exclamationCount++;
+            }
+        }
+        return exclamationCount;
+    }
+
+    /** Returns the number of elements in the numList array that have the same
+     *  value as the element immediately adjacent to the right in the array.
+     *  (BE CAREFUL not to go out of bounds!)
+     *  For example, given the array {4, 7, 7, 3, 2, 2, 4, 4, 4, 6, 4, 6, 6}
+     *  this method would return 5, since there are five numbers in the array that
+     *  are equal to the number immediately to the right (shown below in blue):
+     *                    {4, 7, 7, 3, 2, 2, 4, 4, 4, 6, 4, 6, 6}
+     *
+     *  Does NOT mutate (modify) numList
+     *  PRECONDITION: numList.length > 0
+     *
+     *  @param numList  original array of numbers; does not get modified
+     *  @return  number of elements in numList that are equal to the element
+     *           adjacent to the right of the element in the array
+     */
+    public static int countConsecutiveDoubles(int[] numList)
+    {
+        int count = 0;
+        for (int i = 0; i < numList.length-1; i++)
+        {
+            if (numList[i] == numList[i+1])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /** Returns a NEW array that contains all elements of the original numList,
+     *  shifted to the left by one index; the first element of the array should
+     *  wrap around and appear at the end.
+     *  For example, given the array {5, 1, 3, 4, 7}, this method returns the
+     *  array {1, 3, 4, 7, 5}
+     *
+     *  Does NOT mutate (modify) numList
+     *  PRECONDITION: numList.length > 0
+     *
+     *  @param numList  original array of numbers; does not get modified
+     *  @return  array with elements in numList shifted by one index left
+     */
+    public static int[] shiftLeft(int[] numList)
+    {
+        int[] leftShiftedNumList = new int[numList.length];
+        for (int i = 0; i < numList.length - 1; i++)
+        {
+            leftShiftedNumList[i] = numList[i+1];
+        }
+        leftShiftedNumList[numList.length-1] = numList[0];
+        return leftShiftedNumList;
+    }
+
+    /** Shifts all elements in the array to the left one index (just like in
+     * the previous method), except you are modifying the ORIGINAL array
+     * (numList) and returning nothing (void).
+     *
+     * @param numList
+     */
+    public static void shiftLeftModify(int[] numList)
+    {
+        int[] shiftedList = shiftLeft(numList);
+        for (int i = 0; i < numList.length; i++)
+        {
+            numList[i] = shiftedList[i];
+        }
+    }
 }
+
